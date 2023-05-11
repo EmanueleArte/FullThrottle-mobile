aed29e3eabee218ec75bfff2aeb92fe812713711
Roberto Draghetti <roberto.draghetti@studio.unibo.it>
2023-05-11 18:26:31 +0200

    added empty home screen, navigation bar and top bar

--- FullThrottle/app/src/main/java/com/example/fullthrottle/ui/HomeScreen.kt
+++ FullThrottle/app/src/main/java/com/example/fullthrottle/ui/HomeScreen.kt
@@ -1,2 +1,26 @@
 package com.example.fullthrottle.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen() {
+    val context = LocalContext.current
+    Scaffold(
+        topBar = { TopBar() },
+        bottomBar = { BottomBar() }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+        ) {
+
+        }
+    }
+}
--- FullThrottle/app/src/main/java/com/example/fullthrottle/ui/UiUtils.kt
+++ FullThrottle/app/src/main/java/com/example/fullthrottle/ui/UiUtils.kt
@@ -1,15 +1,18 @@
 package com.example.fullthrottle.ui
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import com.example.fullthrottle.R
 
 @Composable
 fun OutLineTextField(label: String) {
@@ -31,5 +34,53 @@ fun SimpleButton(value: String, onClick: () -> Unit) {
         shape = RoundedCornerShape(10.dp)
     ) {
         Text(value)
+    }
+}
+
+@Composable
+fun TopBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Image(
+                painter = painterResource(id = R.drawable.fullthrottle_logo_light),
+                contentDescription = "app logo",
+                modifier = Modifier
+                    .requiredHeight(50.dp),
+                contentScale = ContentScale.Fit
+            )
+        },
+        navigationIcon = { Icon(Icons.Outlined.ArrowBack, contentDescription = null) },
+        actions = { Icon(Icons.Outlined.Notifications, contentDescription = null) }
+    )
+}
+
+@Composable
+fun BottomBar() {
+    NavigationBar() {
+        NavigationBarItem(
+            icon = { Icon(Icons.Outlined.Home, contentDescription = null) },
+            selected = true,
+            onClick = { /*TODO*/ }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Outlined.Place, contentDescription = null) },
+            selected = false,
+            onClick = { /*TODO*/ }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Outlined.Add, contentDescription = null) },
+            selected = false,
+            onClick = { /*TODO*/ }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Outlined.Search, contentDescription = null) },
+            selected = false,
+            onClick = { /*TODO*/ }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Outlined.Info, contentDescription = null) },
+            selected = false,
+            onClick = { /*TODO*/ }
+        )
     }
 }
